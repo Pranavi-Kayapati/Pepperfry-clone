eefb0cbbe9168a3c5e1379c658fb18c8b9cbc984
Pranavi-Kayapati <pranavikayapati01@gmail.com>
2023-01-18 22:39:42 +0530

    day-2

---
+++ index.js
@@ -0,0 +1,19 @@
+// let slideimg=["https://ii3.pepperfry.com/media/wysiwyg/banners/Hp_section2_web_04012023_2.jpg",
+// "https://ii2.pepperfry.com/media/wysiwyg/banners/Hp_section2_web_04012023_1.jpg",
+// "https://ii1.pepperfry.com/media/wysiwyg/banners/Hp_section2_web_04012023_3.jpg"]
+// let curr_img=0;
+// function slideShow(){
+//   let img=document.createElement("img")
+//   img.src=slideimg[curr_img];
+//   if(curr_img<slideimg.length-1){
+//     curr_img++;
+//   }else{
+//     curr_img=0;
+//   }
+//   slide.append(img);
+// }
+
+// setTimeout("slideShow()",4000);
+// window.onload=slideShow;
+
+console.log("I am pranavi")
